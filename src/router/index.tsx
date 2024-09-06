590bd3ac365e0a10d49b479da4f39b3f7e7680fd
kirill <checkster98@gmail.com>
2024-09-06 15:31:38 +0400

    master feat: added global router

---
+++ src/router/index.tsx
@@ -0,0 +1,13 @@
+import {createBrowserRouter, Navigate} from 'react-router-dom';
+import App from "../App.tsx";
+import modules from "@modules/index";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Navigate to="garage" replace/>
+  },
+  ...modules.router
+]);
+
+export default router;
