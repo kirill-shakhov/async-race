aa92075deb27bbc9bf7ef008b6a6e9b868f737c8
kirill <checkster98@gmail.com>
2024-09-06 13:37:12 +0400

    master feat: added alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('src', import.meta.url)),
+      '@modules': fileURLToPath(new URL('src/modules', import.meta.url)),
+      '@moduleGarage': fileURLToPath(new URL('src/modules/garage', import.meta.url)),
+      '@moduleWinners': fileURLToPath(new URL('src/modules/winners', import.meta.url)),
+    },
+    extensions: ['.tsx', '.ts', '.jsx', '.js']
+  }
 })
