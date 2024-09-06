f2ed81352c4b003abaeb889358e41b29ef8a3893
kirill <checkster98@gmail.com>
2024-09-06 15:32:48 +0400

    master init: created GarageView

---
+++ src/modules/garage/views/GarageView.tsx
@@ -0,0 +1,12 @@
+import AppHeader from "@/shared/components/AppHeader/AppHeader.tsx";
+
+const GarageView = () => {
+  return (
+    <>
+      <AppHeader/>
+      GarageView
+    </>
+  )
+}
+
+export default GarageView;
