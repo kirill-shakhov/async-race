e0fc06aa1cd5018b077e1cf13f6e4f32c49878fd
kirill <checkster98@gmail.com>
2024-09-06 15:33:09 +0400

    master init: created WinnersView

---
+++ src/modules/winners/views/WinnersView.tsx
@@ -0,0 +1,12 @@
+import AppHeader from "@/shared/components/AppHeader/AppHeader.tsx";
+
+const WinnersView = () => {
+  return (
+    <>
+      <AppHeader/>
+      WinnersView
+    </>
+  )
+}
+
+export default WinnersView;
