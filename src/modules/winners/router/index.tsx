0a25e08c6e5f753cfde2d5fe79b7a070087a1e2b
kirill <checkster98@gmail.com>
2024-09-06 15:30:17 +0400

    master feat: added winners route

---
+++ src/modules/winners/router/index.tsx
@@ -0,0 +1,10 @@
+import WinnersView from "../views/WinnersView.tsx";
+
+import {RouteObject} from "react-router-dom";
+
+export default [
+  {
+    path: 'winners',
+    element: <WinnersView/>,
+  }
+] as RouteObject[];
