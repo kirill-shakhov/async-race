ec4c966053d5155700d483db0bd124c491c5e2f3
kirill <checkster98@gmail.com>
2024-09-06 15:30:05 +0400

    master feat: added garage route

---
+++ src/modules/garage/router/index.tsx
@@ -0,0 +1,10 @@
+import GarageView from "../views/GarageView.tsx";
+
+import {RouteObject} from "react-router-dom";
+
+export default [
+  {
+    path: 'garage',
+    element: <GarageView/>,
+  }
+] as RouteObject[];
