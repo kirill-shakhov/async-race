d79582c1bdcf248742b5a737982960c55948b36c
kirill <checkster98@gmail.com>
2024-09-11 17:19:39 +0400

    master feat: added 2 packages '@types/react-redux', '@heroicons'

---
+++ src/services/api/controllers/asyncRaceApi/index.ts
@@ -0,0 +1,14 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+
+// Создаем общий API slice
+export const asyncRaceApi = createApi({
+  reducerPath: 'asyncRaceApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${import.meta.env.VITE_API_URL}`,
+    prepareHeaders: (headers, {getState}) => {
+      headers.set('Content-Type', 'application/json');
+      return headers;
+    },
+  }),
+  endpoints: () => ({}),
+});
