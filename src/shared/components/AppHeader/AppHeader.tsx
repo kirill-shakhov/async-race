9d78c2fc1f388cb410e9e47ed8094b815e084238
kirill <checkster98@gmail.com>
2024-09-12 12:39:31 +0400

    master feat: added navigation and heading

--- src/shared/components/AppHeader/AppHeader.tsx
+++ src/shared/components/AppHeader/AppHeader.tsx
@@ -2,16 +2,28 @@ import {Link} from "react-router-dom";
 
 const AppHeader = () => {
   return (
-    <header>
-      <Link
-        to="/garage">
-        garage
-      </Link>
+    <header className='mt-4'>
+      <nav className='flex justify-between'>
+        <ul className="flex justify-center gap-4 sm:justify-start">
+          <li>
+            <Link
+              to="/garage">
+              garage
+            </Link>
+          </li>
 
-      <Link
-        to="/winners">
-        winners
-      </Link>
+          <li>
+            <Link
+              to="/winners">
+              winners
+            </Link>
+          </li>
+        </ul>
+
+        <h1>
+          Async Race
+        </h1>
+      </nav>
     </header>
   )
 }
