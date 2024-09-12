0be534026e921f49e849566e9e1ecb64c7b9636c
kirill <checkster98@gmail.com>
2024-09-12 12:41:41 +0400

    master feat: added buttons-group 'race', 'reset'

--- src/modules/garage/views/GarageView.tsx
+++ src/modules/garage/views/GarageView.tsx
@@ -1,7 +1,23 @@
+import {UiButton} from "@/shared/components/UI/UiButton";
+import {PlayIcon, XMarkIcon} from "@heroicons/react/16/solid";
+
 const GarageView = () => {
   return (
     <div className="garage-view">
-      GarageView
+      <div className='flex gap-2'>
+        <UiButton>
+          Race <PlayIcon className="ml-1 size-3"/>
+        </UiButton>
+
+        <UiButton
+          theme='danger'
+        >
+          Reset
+          <XMarkIcon className="ml-1 size-3"/>
+        </UiButton>
+      </div>
+
+
     </div>
   )
 }
