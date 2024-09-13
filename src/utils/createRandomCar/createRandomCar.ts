1cd39c06fbb42f13502f6909dec1e3d9c0df6da9
kirill <checkster98@gmail.com>
2024-09-13 11:31:45 +0400

    master feat: added util 'createRandomCar';

---
+++ src/utils/createRandomCar/createRandomCar.ts
@@ -0,0 +1,17 @@
+import {CarWithoutId} from "@moduleGarage/static/types";
+import {getRandomColor} from "@/utils/getRandomColor/getRandomColor.ts";
+
+const firstPartNames = ['Tesla', 'Ford', 'BMW', 'Audi', 'Toyota', 'Mercedes', 'Honda', 'Chevrolet', 'Kia', 'Nissan'];
+const secondPartNames = ['Model S', 'Mustang', 'Civic', 'Corolla', 'Focus', 'Accord', 'Camaro', 'Optima', 'Altima', 'X5'];
+
+
+export const createRandomCar = (): CarWithoutId => {
+  const randomFirstPart = firstPartNames[Math.floor(Math.random() * firstPartNames.length)];
+  const randomSecondPart = secondPartNames[Math.floor(Math.random() * secondPartNames.length)];
+  const randomColor = getRandomColor();
+
+  return {
+    name: `${randomFirstPart}${randomSecondPart}`,
+    color: randomColor,
+  }
+}
