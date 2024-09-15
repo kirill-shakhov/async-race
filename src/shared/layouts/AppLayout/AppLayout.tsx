2035fc15a4a6808f7987e4d6a908ec555b3fea6e
kirill <checkster98@gmail.com>
2024-09-15 23:25:50 +0400

    master fix: changed class to className

--- src/shared/layouts/AppLayout/AppLayout.tsx
+++ src/shared/layouts/AppLayout/AppLayout.tsx
@@ -3,7 +3,7 @@ import {Outlet} from "react-router-dom";
 
 const AppLayout = () => {
   return (
-    <div class="max-w-screen-xl min-h-screen mx-auto px-4 flex flex-col gap-8">
+    <div className="max-w-screen-xl min-h-screen mx-auto px-4 flex flex-col gap-8">
       <AppHeader/>
       <Outlet/>
     </div>
