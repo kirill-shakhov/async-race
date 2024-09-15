754971e8549a64d17f640031e081b3cf3203d8c6
kirill <checkster98@gmail.com>
2024-09-15 22:52:49 +0400

    master feat: added creating one car

--- src/modules/garage/views/useGarageView.tsx
+++ src/modules/garage/views/useGarageView.tsx
@@ -1,13 +1,26 @@
+import * as Yup from 'yup';
+
 import {CarWithoutId} from "@moduleGarage/static/types";
-import {useCreateCarMutation, useGetCarsQuery} from "@/services/api/controllers/asyncRaceApi/modules/carApi";
+import {
+  useCreateCarMutation,
+  useDeleteCarMutation,
+  useGetCarsQuery
+} from "@/services/api/controllers/asyncRaceApi/modules/carApi";
 import {createRandomCar} from "@/utils/createRandomCar/createRandomCar.ts";
 import {setCars, setTotalCount} from "@moduleGarage/store";
 import {useAppDispatch} from "@/store/hooks.ts";
-import {useState} from "react";
+import {ChangeEvent, useState} from "react";
+
+interface ErrorState {
+  name: string;
+  color: string;
+}
+
 
 const useGarageView = () => {
   const [createCar] = useCreateCarMutation();
   const {refetch} = useGetCarsQuery({page: 1, limit: 7});
+  const [deleteCar]= useDeleteCarMutation();
 
   const [isLoadingCreatedCars, setIsLoadingCreatedCars] = useState(false);
 
@@ -44,9 +57,85 @@ const useGarageView = () => {
     }
   }
 
+
+  // create a car
+  const validationSchema = Yup.object().shape({
+    name: Yup.string()
+      .min(2, 'Name must be at least 2 characters')
+      .max(50, 'Name must be less than or equal to 50 characters')
+      .required('Name is required'),
+    color: Yup.string()
+      .matches(/^#[0-9A-F]{6}$/i, 'Color must be in HEX format')
+      .required('Color is required'),
+  });
+
+  const [values, setValues] = useState({name: '', color: '#ffffff'});
+  const [errors, setErrors] = useState({name: '', color: ''});
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const {name, value} = e.target;
+
+    setValues({
+      ...values,
+      [name]: value,
+    });
+
+    if (errors[name as keyof ErrorState]) {
+      setErrors({
+        ...errors,
+        [name]: '',
+      });
+    }
+  };
+
+  // Handle form submission
+  const handleSubmit = async (e: Event) => {
+    e.preventDefault();
+    try {
+      await validationSchema.validate(values, {abortEarly: false});
+      console.log('Validated Data:', values);
+    } catch (err: any) {
+      const validationErrors: Partial<ErrorState> = {};
+      err.inner.forEach((error: any) => {
+        validationErrors[error.path as keyof ErrorState] = error.message;
+      });
+      setErrors({
+        ...errors,
+        ...validationErrors,
+      });
+    }
+  };
+
+//delete car
+  const sendCarDeleteRequest = async (id: number): Promise<void> => {
+    try {
+      await deleteCar(id);
+
+      const refetchResponse = await refetch();
+
+      if (refetchResponse.data) {
+        const {cars, totalCount} = refetchResponse.data;
+        dispatch(setCars(cars));
+        dispatch(setTotalCount(totalCount));
+
+        setIsLoadingCreatedCars(false);
+      }
+    }
+
+    catch (e) {
+      console.log(e);
+    }
+  }
+
+
   return {
+    values,
+    errors,
+    handleChange,
+    handleSubmit,
     handleGenerateCars,
-    isLoadingCreatedCars
+    isLoadingCreatedCars,
+    sendCarDeleteRequest
   }
 }
 
