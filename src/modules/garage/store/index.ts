ed0c48eb437caeb041d816e04a06727d543b2401
kirill <checkster98@gmail.com>
2024-09-18 10:06:05 +0400

    master feat: added currentPage, selectedCar in the store

--- src/modules/garage/store/index.ts
+++ src/modules/garage/store/index.ts
@@ -4,11 +4,15 @@ import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 interface GarageState {
   cars: Car[] | null;
   totalCount: number;
+  currentPage: number;
+  selectedCar: Car | null;
 }
 
 const initialState: GarageState = {
   cars: [],
   totalCount: 0,
+  currentPage: 1,
+  selectedCar: null
 }
 
 const garageSlice = createSlice({
@@ -23,14 +27,22 @@ const garageSlice = createSlice({
     },
     setTotalCount: (state, action: PayloadAction<number>) => {
       state.totalCount = action.payload;
-    }
+    },
+    setCurrentPage: (state, action: PayloadAction<number>) => {
+      state.currentPage = action.payload;
+    },
+    setSelectedCar: (state, action: PayloadAction<Car>) => {
+      state.selectedCar = action.payload;
+    },
   }
 })
 
 export const {
   setCars,
   setTotalCount,
-  clearCars
+  clearCars,
+  setCurrentPage,
+  setSelectedCar
 } = garageSlice.actions;
 
 export default garageSlice.reducer;
