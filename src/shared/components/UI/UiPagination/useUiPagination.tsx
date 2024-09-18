1f0f970263469ff7a44ddbf64dc655769485ad35
kirill <checkster98@gmail.com>
2024-09-18 12:52:43 +0400

    master refactor: reduced code duplication by extracting common data fetching logic into useFetchAndUpdateCars hook

--- src/shared/components/UI/UiPagination/useUiPagination.tsx
+++ src/shared/components/UI/UiPagination/useUiPagination.tsx
@@ -1,9 +1,11 @@
 import {useAppDispatch, useAppSelector} from "@/store/hooks.ts";
 import {useGetCarsQuery} from "@/services/api/controllers/asyncRaceApi/modules/carApi";
 import {setCars, setTotalCount, setCurrentPage, clearCars} from "@moduleGarage/store";
+import useFetchAndUpdateCars from "@moduleGarage/hooks/useFetchAndUpdateCars.ts";
 
 const useUiPagination = () => {
   const dispatch = useAppDispatch();
+  const fetchAndUpdateCars = useFetchAndUpdateCars();
 
   let currentPage = useAppSelector(state => state.garage.currentPage);
   let stateTotalCount = useAppSelector(state => state.garage.totalCount);
@@ -12,34 +14,18 @@ const useUiPagination = () => {
 
   let pagesCount = Math.ceil(stateTotalCount / 7);
 
-
-  const getNewCarsList = async (page: number) => {
-    try {
-      await dispatch(clearCars());
-      const refetchResponse = await refetch({page});
-
-      if (refetchResponse.data) {
-        const {cars, totalCount} = refetchResponse.data;
-        dispatch(setCars(cars));
-        dispatch(setTotalCount(totalCount));
-      }
-    } catch (e) {
-      console.error(e);
-    }
-  }
-
   const handlePageClick = async (page: number): Promise<void> => {
     if (page === currentPage) return;
 
     dispatch(setCurrentPage(page));
-    await getNewCarsList(page);
+    await fetchAndUpdateCars(page);
   };
 
   const handlePreviousPage = async (): Promise<void> => {
     if (currentPage > 1) {
       const newPage = currentPage - 1;
       dispatch(setCurrentPage(newPage));
-      await getNewCarsList(newPage);
+      await fetchAndUpdateCars(newPage);
     }
   };
 
@@ -47,7 +33,7 @@ const useUiPagination = () => {
     if (currentPage < pagesCount) {
       const newPage = currentPage + 1;
       dispatch(setCurrentPage(newPage));
-      await getNewCarsList(newPage);
+      await fetchAndUpdateCars(newPage);
     }
   };
 
