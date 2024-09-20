8e1a9f1a6db94a905acadbbfb66e7148ec242b83
kirill <checkster98@gmail.com>
2024-09-20 12:54:34 +0400

    master feat: add custom hook useCarEngineControl

---
+++ src/modules/garage/hooks/useCarEngineControl.ts
@@ -0,0 +1,62 @@
+import {CarStatus, Id} from "@/services/api/controllers/asyncRaceApi/asyncRaceApi.types.ts";
+import {
+  useDriveCarMutation,
+  useStartStopCarEngineMutation
+} from "@/services/api/controllers/asyncRaceApi/modules/carApi";
+import {useState} from "react";
+import useCarAnimation from "@moduleGarage/hooks/useCarAnimation.ts";
+
+export const useCarEngineControl = () => {
+  const [startStopCarEngine] = useStartStopCarEngineMutation();
+  const [driveCar] = useDriveCarMutation();
+  const {startAnimation, stopAnimation} = useCarAnimation();
+
+  const [engineStatus, setEngineStatus] = useState(false);
+
+
+  const startCarEngine = async (id: Id, carRef: HTMLElement) => {
+    try {
+      setEngineStatus(true);
+      const response = await startStopCarEngine({id, status: CarStatus.STARTED});
+
+      if (response?.data) {
+        const {velocity, distance} = response.data;
+
+        const time = distance / velocity;
+        const containerWidth = carRef.parentElement?.offsetWidth || document.body.clientWidth;
+        const carWidth = carRef.offsetWidth;
+        const initialPosition = carRef.offsetLeft || 0;
+        const maxDistance = containerWidth - carWidth - initialPosition;
+        const animationDistance = Math.min(distance, maxDistance);
+
+        startAnimation(carRef, animationDistance, time);
+
+        const driveResponse = await driveCar(id);
+        if (!driveResponse?.data?.success) {
+          console.log('сломалась')
+          stopAnimation();
+        }
+      }
+    } catch (e) {
+      console.log(e);
+      setEngineStatus(false);
+    }
+  };
+
+  const stopCarEngine = async (id: Id, carRef: HTMLElement) => {
+    try {
+      await startStopCarEngine({id, status: CarStatus.STOPPED});
+      setEngineStatus(false);
+      stopAnimation();
+      carRef.style.transform = 'translateX(0px)';
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return {
+    startCarEngine,
+    stopCarEngine,
+    engineStatus
+  }
+}
