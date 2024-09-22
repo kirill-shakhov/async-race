fc1aeb5cd8705e8f44f59f4c016e06c3b3cc4320
kirill <checkster98@gmail.com>
2024-09-22 15:39:47 +0400

    master feat: added useCarEngineControl for driving one car

--- src/modules/garage/components/CarGarageRaceManager/CarGarageRaceManager.tsx
+++ src/modules/garage/components/CarGarageRaceManager/CarGarageRaceManager.tsx
@@ -1,21 +1,25 @@
-import React, {useRef} from "react";
+import React, {useEffect, useRef} from "react";
 import {UiButton} from "@/shared/components/UI/UiButton";
 import {PlayIcon, XMarkIcon} from "@heroicons/react/16/solid";
 import useCarGarageRaceManager from "@moduleGarage/components/CarGarageRaceManager/useCarGarageRaceManager.tsx";
 import {useAppSelector} from "@/store/hooks.ts";
 import {Car} from "@moduleGarage/components";
+import {useCarEngineControl} from "@moduleGarage/hooks/useCarEngineControl.ts";
 
 interface CarGarageRaceManagerProps {
   car: {
     id: number;
     name: string;
     color: string;
-  }
+  };
+  setCarRef: (ref: HTMLElement | null, id: number, index: number) => void;
+  index: number;
 }
 
-const CarGarageRaceManager = ({car}: CarGarageRaceManagerProps) => {
+const CarGarageRaceManager = ({car, setCarRef, index}: CarGarageRaceManagerProps) => {
   let selectedCar = useAppSelector(state => state.garage.selectedCar);
   let selectedCarId = selectedCar ? selectedCar.id : null;
+  const {startCarEngine, stopCarEngine, engineStatus} = useCarEngineControl();
 
   const carRef = useRef<HTMLDivElement | null>(null);
 
@@ -23,13 +27,19 @@ const CarGarageRaceManager = ({car}: CarGarageRaceManagerProps) => {
     sendCarDeleteRequest,
     selectCar,
 
-    engineStatus,
-    startCarEngine,
-    stopCarEngine
   } = useCarGarageRaceManager();
+
+  useEffect(() => {
+    if (carRef.current) {
+      setCarRef(carRef.current, car.id, index);
+    }
+  }, [carRef, car.id, index, setCarRef]);
+
+
 
   return (
     <div className='border-y-2 pt-2 border-gray-300 flex flex-col gap-7'>
+      {JSON.stringify(engineStatus)}
       <div className="flex gap-2">
         <div className='flex gap-2'>
           <div className="buttons-group flex flex-col gap-2">
@@ -54,7 +64,7 @@ const CarGarageRaceManager = ({car}: CarGarageRaceManagerProps) => {
           <div className="buttons-group flex flex-col gap-2">
             <UiButton
               disabled={engineStatus}
-              onClick={() => startCarEngine(car.id, carRef.current!)} // Передаем элемент машины для анимации
+              onClick={() => startCarEngine(car.id, carRef.current!)}
               size='sm'
               block
             >
@@ -62,7 +72,7 @@ const CarGarageRaceManager = ({car}: CarGarageRaceManagerProps) => {
             </UiButton>
 
             <UiButton
-              onClick={() => stopCarEngine(car.id, carRef.current!)}  // Останавливаем анимацию и двигатель
+              onClick={() => stopCarEngine(car.id, carRef.current!)}
               size='sm'
               block
               theme="danger">
