09b223fe970abc5a1a9d2a24354e57193885926b
kirill <checkster98@gmail.com>
2024-09-23 15:37:55 +0400

    master feat: added winners arr in the store 'winners'

--- src/modules/winners/store/index.ts
+++ src/modules/winners/store/index.ts
@@ -2,10 +2,12 @@ import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 import {Winner, WInnerWithoutWins} from "@moduleWinners/static/types";
 
 interface WinnersState {
+  winners: Winner[] | [];
   currentWinner: Winner | null;
 }
 
 const initialState: WinnersState = {
+  winners: [],
   currentWinner: null,
 }
 
@@ -16,11 +18,21 @@ const winnerSlice = createSlice({
     setCurrentWinner: (state, action: PayloadAction<Winner>) => {
       state.currentWinner = action.payload;
     },
+    setWinners: (state, action: PayloadAction<Winner[]>) => {
+      state.winners = action.payload;
+    },
+
+    clearWinners: (state) => {
+      state.winners = [];
+    },
   }
 })
 
 export const {
   setCurrentWinner,
+
+  setWinners,
+  clearWinners
 } = winnerSlice.actions;
 
 export default winnerSlice.reducer;
