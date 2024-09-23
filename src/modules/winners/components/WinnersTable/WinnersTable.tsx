0257641d5d25ea7de06e709332e3bc4fafdb7c2c
kirill <checkster98@gmail.com>
2024-09-23 19:25:37 +0400

    master feat: add table of results winners with selecting sort by option/ by order

--- src/modules/winners/components/WinnersTable/WinnersTable.tsx
+++ src/modules/winners/components/WinnersTable/WinnersTable.tsx
@@ -1,12 +1,40 @@
 import {Car} from "@moduleGarage/components";
-import {useAppSelector} from "@/store/hooks.ts";
+import {useAppDispatch, useAppSelector} from "@/store/hooks.ts";
 import {useEffect, useState} from "react";
 import {useLazyGetCarQuery} from "@/services/api/controllers/asyncRaceApi/modules/carApi";
+import {UiSelect} from "@/shared/components";
+import {SortDirection, SortOptions} from "@/shared/types";
+import {setSortingOption, setSortingOrder} from "@moduleWinners/store";
 
 const WinnersTable = () => {
   const [triggerGetCar] = useLazyGetCarQuery()
+
   const winnersList = useAppSelector(state => state.winners.winners);
+  const sortingDirection = useAppSelector(state => state.winners.sortingDirection);
+  const sortingOption = useAppSelector(state => state.winners.sortingOption);
+
   const [winnersWithInfo, setWinnersWithInfo] = useState([])
+  const dispatch = useAppDispatch();
+
+  const sortOptions = [
+    {value: "id", text: "Sort by ID"},
+    {value: "wins", text: "Sort by Wins"},
+    {value: "time", text: "Sort by Time"},
+  ]
+
+
+  const sortOrders = [
+    {value: SortDirection.ASC, text: "Ascending"},
+    {value: SortDirection.DESC, text: "Descending"}
+  ];
+
+  const handleSortByOrder = (value: SortDirection) => {
+    dispatch(setSortingOrder(value));
+  }
+
+  const handleSortByOption = (value: SortOptions) => {
+    dispatch(setSortingOption(value));
+  }
 
   useEffect(() => {
     const getInfoWinnerFromWinnersList = async () => {
@@ -29,69 +57,83 @@ const WinnersTable = () => {
 
     getInfoWinnerFromWinnersList();
   }, [winnersList, triggerGetCar]);
-  
+
   return (
-    <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-      {JSON.stringify(winnersWithInfo)}
+    <div className='flex flex-col gap-y-7 min-h-screen'>
+      <div className='flex justify-end gap-4'>
+        <UiSelect
+          list={sortOrders}
+          selectedValue={sortingDirection}
+          onChange={handleSortByOrder}
+        />
 
-      {winnersWithInfo.length > 0 && (
-        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-          <tr>
-            <th scope="col" className="px-6 py-3">
-              Number
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Car
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Name
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Wins
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Best time (seconds)
-            </th>
-          </tr>
-          </thead>
-          <tbody>
+        <UiSelect
+          list={sortOptions}
+          selectedValue={sortingOption}
+          onChange={handleSortByOption}
+        />
+      </div>
 
-          {winnersWithInfo.map((winner) => (
-            <tr
-              key={winner.id}
-              className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-              <td className="w-4 p-4">
-                <div className="flex items-center">
-                  {winner.id}
-                </div>
-              </td>
-
-              <th scope="row"
-                  className="flex items-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                <Car color={winner.color}/>
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg gap-2">
+        {winnersWithInfo.length > 0 && (
+          <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Number
               </th>
-
-              <td className="px-6 py-4">
-                {winner.carName}
-              </td>
-
-              <td className="px-6 py-4">
-                <div className="flex items-center">
-                  {winner.wins}
-                </div>
-              </td>
-              <td className="px-6 py-4">
-                <p>
-                  {winner.time}
-                </p>
-              </td>
+              <th scope="col" className="px-6 py-3">
+                Car
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Name
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Wins
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Best time (seconds)
+              </th>
             </tr>
-          ))}
-          </tbody>
-        </table>
-      )}
+            </thead>
+            <tbody>
 
+            {winnersWithInfo.map((winner) => (
+              <tr
+                key={winner.id}
+                className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <td className="w-4 p-4">
+                  <div className="flex items-center">
+                    {winner.id}
+                  </div>
+                </td>
+
+                <th scope="row"
+                    className="flex items-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                  <Car color={winner.color}/>
+                </th>
+
+                <td className="px-6 py-4">
+                  {winner.carName}
+                </td>
+
+                <td className="px-6 py-4">
+                  <div className="flex items-center">
+                    {winner.wins}
+                  </div>
+                </td>
+                <td className="px-6 py-4">
+                  <p>
+                    {winner.time}
+                  </p>
+                </td>
+              </tr>
+            ))}
+            </tbody>
+          </table>
+        )}
+
+      </div>
     </div>
   )
 }
