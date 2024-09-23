c0eb82dca5a49eadbd01ec08688f861c536be61b
kirill <checkster98@gmail.com>
2024-09-23 15:38:58 +0400

    master feat: add getting winners arr when page is rendered

--- src/modules/winners/views/WinnersView.tsx
+++ src/modules/winners/views/WinnersView.tsx
@@ -1,9 +1,41 @@
 import {WinnersTable} from "@moduleWinners/components";
+import {useLazyGetWinnersQuery} from "@/services/api/controllers/asyncRaceApi/modules/winnerApi";
+import {useEffect} from "react";
+import {useAppDispatch, useAppSelector} from "@/store/hooks.ts";
+import {clearWinners, setWinners} from "@moduleWinners/store";
+import {
+  DEFAULT_WINNERS_PER_PAGE,
+  INITIAL_WINNERS_PAGE
+} from "@/services/api/controllers/asyncRaceApi/modules/winnerApi/WinnersApi.constants.ts";
 
 const WinnersView = () => {
+  const [triggerGetWinners, {data: winners}] = useLazyGetWinnersQuery();
+  const winnersList = useAppSelector(state => state.winners.winners);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    return () => {
+      dispatch(clearWinners());
+    };
+  }, [dispatch]);
+
+
+  useEffect(() => {
+    triggerGetWinners({
+      page: INITIAL_WINNERS_PAGE,
+      limit: DEFAULT_WINNERS_PER_PAGE,
+      sort: 'id',
+      order: 'ASC'
+    }).unwrap().then((response) => {
+      if (response && winnersList.length === 0) {
+        dispatch(setWinners(response));
+      }
+    });
+  }, [winnersList.length, dispatch]);
+
   return (
     <div className='flex flex-col gap-y-14 min-h-screen'>
-      <WinnersTable/>
+      {winners && winnersList.length > 0 && (<WinnersTable/>)}
     </div>
   )
 }
