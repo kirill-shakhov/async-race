9fe444753edca35d397100dc20c089d01ccd398f
kirill <checkster98@gmail.com>
2024-09-24 15:53:36 +0400

    master feat: refetch cars and decrease current page if array is empty

--- src/modules/garage/hooks/useFetchAndUpdateCars.ts
+++ src/modules/garage/hooks/useFetchAndUpdateCars.ts
@@ -1,6 +1,6 @@
 import {useCallback} from 'react';
 import {useAppDispatch, useAppSelector} from '@/store/hooks.ts';
-import {clearCars, setCars, setTotalCount} from '@moduleGarage/store';
+import {clearCars, setCars, setCurrentPage, setTotalCount} from '@moduleGarage/store';
 import {useLazyGetCarsQuery} from '@/services/api/controllers/asyncRaceApi/modules/carApi';
 import {DEFAULT_GARAGE_CARS_PER_PAGE} from "@/services/api/controllers/asyncRaceApi/modules/carApi/CarApi.constants.ts";
 
@@ -18,15 +18,22 @@ const useFetchAndUpdateCars = () => {
         limit: DEFAULT_GARAGE_CARS_PER_PAGE,
       }).unwrap();
 
+      if (result.cars.length === 0 && currentPage > 1) {
+        dispatch(setCurrentPage(currentPage - 1));
+        await fetchAndUpdateCars(currentPage - 1);
+        return;
+      }
       if (result) {
         const {cars, totalCount} = result;
         dispatch(setCars(cars));
         dispatch(setTotalCount(totalCount));
       }
+
+
     } catch (e) {
       console.error(e);
     }
-  }, [dispatch, triggerGetCarsQuery, currentPage]);
+  }, [dispatch, currentPage]);
 
   return fetchAndUpdateCars;
 };
