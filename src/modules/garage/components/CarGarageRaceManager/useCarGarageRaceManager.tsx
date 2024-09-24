37c127b028aed83128bebaed4c78c79e18dfcedd
kirill <checkster98@gmail.com>
2024-09-24 15:12:56 +0400

    master feat: add useFetchAndUpdateCars for updating winner and total count

--- src/modules/garage/components/CarGarageRaceManager/useCarGarageRaceManager.tsx
+++ src/modules/garage/components/CarGarageRaceManager/useCarGarageRaceManager.tsx
@@ -1,38 +1,29 @@
-import {setCars, setTotalCount, setSelectedCar} from "@moduleGarage/store";
+import {setSelectedCar} from "@moduleGarage/store";
 import {
   useDeleteCarMutation,
-  useGetCarsQuery,
 } from "@/services/api/controllers/asyncRaceApi/modules/carApi";
 
 import {useAppDispatch, useAppSelector} from "@/store/hooks.ts";
 
-import {DEFAULT_GARAGE_CARS_PER_PAGE} from "@/services/api/controllers/asyncRaceApi/modules/carApi/CarApi.constants.ts";
 import {Car} from "@moduleGarage/static/types";
-import {useCarEngineControl} from "@moduleGarage/hooks/useCarEngineControl.ts";
 import {removeWinner} from "@moduleWinners/store";
 import {useDeleteWinnerMutation} from "@/services/api/controllers/asyncRaceApi/modules/winnerApi";
+import useFetchAndUpdateCars from "@moduleGarage/hooks/useFetchAndUpdateCars.ts";
 
 const useCarGarageRaceManager = () => {
   const dispatch = useAppDispatch();
   const [deleteCar] = useDeleteCarMutation();
 
   let currentPage = useAppSelector(state => state.garage.currentPage);
-  const {refetch} = useGetCarsQuery({page: currentPage, limit: DEFAULT_GARAGE_CARS_PER_PAGE});
   const [deleteWinner] = useDeleteWinnerMutation();
+  const fetchAndUpdateCars = useFetchAndUpdateCars();
 
 
   const sendCarDeleteRequest = async (id: number): Promise<void> => {
     try {
       await deleteCar(id);
       await deleteWinner(id);
-
-      const refetchResponse = await refetch();
-
-      if (refetchResponse.data) {
-        const {cars, totalCount} = refetchResponse.data;
-        dispatch(setCars(cars));
-        dispatch(setTotalCount(totalCount));
-      }
+      await fetchAndUpdateCars(currentPage);
 
       dispatch(removeWinner(id));
     } catch (e) {
