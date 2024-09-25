da347b642e511eed9651079a19586224244b7cee
kirill <checkster98@gmail.com>
2024-09-25 11:48:22 +0400

    master fix: resolve formatting and linting issues reported by Prettier and ESLint

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
 import './index.scss';
 
-import router from '@/router';
 import { RouterProvider } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
 import { store } from '@/store';
 
+import router from '@/router';
+
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <RouterProvider router={router} />
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
   </Provider>,
 );
