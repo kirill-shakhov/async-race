99ce0278eaf123f4753fe54e0e4526a2da91ea4a
kirill <checkster98@gmail.com>
2024-09-25 18:11:12 +0400

    master feat: add styles for Link and heading 1

--- src/shared/components/AppHeader/AppHeader.tsx
+++ src/shared/components/AppHeader/AppHeader.tsx
@@ -3,18 +3,29 @@ import { Link } from 'react-router-dom';
 function AppHeader() {
   return (
     <header className="mt-4">
-      <nav className="flex justify-between">
+      <nav className="flex justify-between items-center">
         <ul className="flex justify-center gap-4 sm:justify-start">
           <li>
-            <Link to="/garage">garage</Link>
+            <Link
+              to="/garage"
+              className="text-gray-800 hover:text-blue-700 font-semibold"
+            >
+              Garage
+            </Link>
           </li>
-
           <li>
-            <Link to="/winners">winners</Link>
+            <Link
+              to="/winners"
+              className="text-gray-800 hover:text-blue-700 font-semibold"
+            >
+              Winners
+            </Link>
           </li>
         </ul>
 
-        <h1>Async Race</h1>
+        <h1 className="text-xl font-bold text-gray-800">
+          Async Race
+        </h1>
       </nav>
     </header>
   );
